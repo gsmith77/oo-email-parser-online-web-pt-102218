70b7a25062163252f458acb8f7c490614c2df776
grant smith <grantsmith1256@gmail.com>
2018-11-17 20:45:36 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,12 @@ class EmailParser
     @emails = emails
   end
   
-
+  def parse
+    if emails.include?(", ")
+    emails.split(", ").uniq
+    else emails.include?(" ")
+    emails.split(", ").uniq
+  end
   
   
   
